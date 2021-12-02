b196ea5ec0f90bc7369b32fca13f3361e0ba2fa9
yzr95924 <zryang@cse.cuhk.edu.hk>
2021-12-03 03:26:36 +0800

    update

---
+++ script/recompile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf ./bin/*
+cd ./build
+cmake ..
+#make -j$(shell grep -c ^processor /proc/cpuinfo 2>/dev/null)
+make -j4
+cd ..
+cd ./bin
+# mkdir Containers Recipes input output Meta
+cd ..
+#cp config.json ./bin
---
+++ src/app/main.cc
@@ -0,0 +1,19 @@
+/**
+ * @file main.cc
+ * @author Zuoru YANG (zryang@cse.cuhk.edu.hk)
+ * @brief the main template
+ * @version 0.1
+ * @date 2021-12-03
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    /* code */
+    uint64_t* test = (uint64_t*) malloc(sizeof(uint64_t));
+    return 0;
+}
