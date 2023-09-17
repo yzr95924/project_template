830cd35fd15ca7ff8dec8b3ab989c7a46d536647
yzr95924 <yzr95924@gmail.com>
2023-09-17 18:16:18 +0800

    update kernel build

---
+++ cmake/FindKernelHeader.cmake
@@ -0,0 +1,28 @@
+# Find the kernel release
+execute_process(
+        COMMAND uname -r
+        OUTPUT_VARIABLE KERNEL_RELEASE_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+# Find the headers
+find_path(KERNEL_SOURCE_DIR
+    include/linux/user.h
+    PATHS /usr/src/linux-headers-${KERNEL_RELEASE_VERSION}
+)
+
+message(STATUS "Kernel release: ${KERNEL_RELEASE_VERSION}")
+message(STATUS "Kernel headers: ${KERNEL_SOURCE_DIR}")
+
+if (KERNEL_SOURCE_DIR)
+    set(KERNEL_HEADER_INCLUDE
+        ${KERNEL_SOURCE_DIR}/include
+        ${KERNEL_SOURCE_DIR}/arch/x86/include
+        CACHE PATH "Kernel headers include dirs"
+    )
+    set(KERNEL_HEADER_FOUND 1 CACHE STRING "Set to 1 if kernel headers were found")
+else (KERNEL_SOURCE_DIR)
+    set(KERNEL_HEADER_FOUND 0 CACHE STRING "Set to 1 if kernel headers were found")
+endif (KERNEL_SOURCE_DIR)
+
+mark_as_advanced(KERNEL_HEADER_FOUND)
